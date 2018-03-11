f74fea93bbc545401bc7cd8e40fc0b1a24804f18
dietrich313 <dietrich313@gmail.com>
2018-03-11 03:24:13 +0200

    BIG CHANGE
    313

--- Assets/Sources/Controller/PlayerController.cs
+++ Assets/Sources/Controller/PlayerController.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-
+    Animator movement;
+    private SpriteRenderer spr;
+    bool sideSwitched;
     private Rigidbody2D playerRB2D;
 
 	
 	void Start () {
-        transform.position = new Vector2(-Screen.width, Screen.height);
+
+        spr = GetComponent<SpriteRenderer>();
         playerRB2D = GetComponent<Rigidbody2D>();
+        movement = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-		
-	}
+        
+      
+        playerRB2D.velocity = new Vector2(moveHorizontal*speed,moveVertical*speed);
+        if ((playerRB2D.velocity.x != 0f) || (playerRB2D.velocity.y != 0f))
+        {
+            movement.Play("walk1");
+            if (moveHorizontal < 0 && sideSwitched==false) {  spr.flipX = true; sideSwitched = true; } // rotate player to walk back
+            else if(moveHorizontal>0 && sideSwitched == true) { spr.flipX = false; sideSwitched = false; }
+        }
+        else movement.Play("idle");
+    }
 }
