3e62aa4b176339c40d4ea9bfec466d50c24b83f8
dietrich313 <dietrich313@gmail.com>
2018-03-11 06:39:40 +0200

    Scorer
    ne izkarva score v game view

---
+++ Assets/Sources/UI/ScoreCounter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+
+    public Text scoreBoard;
+	private int Score=0;
+
+	public void ScoreCount(int givenPoints)
+    {
+        Score += givenPoints;
+        scoreBoard.text = "Score: " + Score;
+    }
+}
