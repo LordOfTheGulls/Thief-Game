a998c3516e9f8660bde1cc2111dfcbcfcd3d8161
dietrich313 <dietrich313@gmail.com>
2018-03-16 20:41:06 +0200

    Menu 1.2
    Added animation fo Window Lightning needs the time to be tweaked

---
+++ Assets/Sources/UI/WindowLights.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+
+public class WindowLights : MonoBehaviour
+{
+
+    public Image[] ImageList = new Image[10];
+    void Start()
+    {
+
+        InvokeRepeating("LightWindows", 0, 5);
+        InvokeRepeating("DelayWindows", 0, 8);
+
+    }
+
+    void LightWindows()
+    {
+        int n = Random.Range(0, 10);
+        ImageList[n].enabled = !ImageList[n].enabled;
+
+
+    }
+
+    void DelayWindows()
+    {
+        int n = Random.Range(0, 10);
+        ImageList[n].enabled = false;
+
+    }
+}
+
+
